721a9c58ab13fd8c3013a4df75157ba2e458d685
Gustavo Cunha Santos <105940671+Gustavo13Cs@users.noreply.github.com>
2024-03-22 10:58:16 -0300

    .

---
+++ AplicaçãoEmMVCcore/AplicaçãoEmMVCcore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AplicaçãoEmMVCcore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

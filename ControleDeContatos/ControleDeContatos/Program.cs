5ac0ccb2eb7df45e92262480180f64d35d2c57ba
Gustavo Cunha Santos <105940671+Gustavo13Cs@users.noreply.github.com>
2024-03-29 16:14:38 -0300

    .

--- ControleDeContatos/ControleDeContatos/Program.cs
+++ ControleDeContatos/ControleDeContatos/Program.cs
@@ -1,13 +1,33 @@
+using ControleDeContatos.Data;
+using ControleDeContatos.Models;
+using ControleDeContatos.Repositorio;
+using Microsoft.EntityFrameworkCore;
+
 namespace ControleDeContatos
 {
     public class Program
     {
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+
+            var provider = services.BuildServiceProvider();
+            var configurantion = provider.GetRequiredService<IConfiguration>();
+            services.AddDbContext<BancoContext>(item => item.UseSqlServer(configurantion.GetConnectionString("DataBase")));
+            services.AddScoped<IContatoRepositorio, ContatoRepositorio>();
+            services.AddAuthorization();
+            services.AddAuthentication();
+        }
+
+        
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            builder.Services.AddControllersWithViews();
+
+
 
             var app = builder.Build();
 
---
+++ ControleDeContatos/ControleDeContatos/Repositorio/ContatoRepositorio.cs
@@ -0,0 +1,22 @@
+﻿using ControleDeContatos.Data;
+using ControleDeContatos.Models;
+
+namespace ControleDeContatos.Repositorio
+{
+    public class ContatoRepositorio : IContatoRepositorio
+    {
+        private readonly BancoContext _bancoContext;
+        public ContatoRepositorio(BancoContext bancoContext)
+        {
+            _bancoContext = bancoContext;
+        }
+        public ContatoModel Adicionar(ContatoModel contato)
+        {
+            // gravar no banco de dados
+
+            _bancoContext.Contatos.Add(contato);
+            _bancoContext.SaveChanges(); //e como se fosse uma confirmação
+            return contato;
+        }
+    }
+}
